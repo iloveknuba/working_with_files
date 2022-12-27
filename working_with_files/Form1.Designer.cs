0cc4b0d4d3fbcd239a72208b6ad7445ab58661be
iloveknuba <xxxcccer982@gmail.com>
2022-12-27 15:37:25 +0200

    Добавьте файлы проекта.

---
+++ working_with_files/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace working_with_files
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(74, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(614, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(74, 210);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(614, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(74, 324);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(614, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "File path:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Array in file:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 288);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(258, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Amount of elements than enters only 1 time";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(694, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(71, 397);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ working_with_files/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace working_with_files
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Choose the .txt file";
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+            textBox1.Text = openFileDialog1.FileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            File f = new File();
+            string s = textBox1.Text;
+            f.st(s, textBox2);
+            f.res(textBox3);
+        }
+        class File
+        {
+            int[] a;
+            int[] b;
+            int length;
+            public bool f = false;
+            int k1 = 0;
+            int k = 0;
+            public File()
+            {
+                a = new int[100];
+                b = new int[100];
+                length = 100;
+            }
+            void show(TextBox t, int[] aa)
+            {
+                t.Text = "";
+                for (int i = 0; i < length; i++)
+                {
+                    t.AppendText(aa[i] + "    ");
+                }
+            }
+
+            public void st(string s, TextBox t)
+            {
+                int i = 0;
+                using (StreamReader sr = new StreamReader(s, System.Text.Encoding.Default)) // reading lines with array elements in txt file
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        a[i] = Convert.ToInt16(line);
+                        i++;
+                    }
+                }
+                length = i;
+                show(t, a);
+            }
+
+
+            public void res(TextBox t) 
+            {
+                int[] b1;
+                for (int i = 0; i < length; i++)
+                {
+                    int k = 0;
+                    for (int j = 0; j < length; j++)
+                    {
+                        if (a[i] == a[j] && i != j)
+                        {
+                            k1++;
+                            break;
+
+                        }
+
+                    }
+                    k = length - k1;
+                    t.Text = Convert.ToString(k);
+                    string s = @"D:\недоУчоба\2 курс\ООП\working_with_files\raz.txt";
+                    using (StreamWriter sw = new StreamWriter(s, false, System.Text.Encoding.Default)) // writing result in another txt file
+                    {
+
+                        sw.WriteLine("Amount of elements that happens in array 1 time only:   " + k.ToString());
+
+                    }
+
+                }
+
+
+            }
+
+        }
+    }
+}
